2df6bff4c893eb1b9a7da5351551c9fda68b4cb9
Carlos Lancha <carlitoslancha@gmail.com>
2021-10-16 14:06:45 +0200

    feat: create mod

---
+++ NoDeath/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoDeath")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoDeath")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B027A19A-CD89-4FD4-AE2D-11ED68A5D913")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # no-death-mod
+
+This mod prevents your character from dead. You'll get the damage and the damage animation will happen, but if the hit deals you enough amount of damage to kill you won't dead.
+
+This is useful in NoHit runs to continue it without having to restart the game or load a save.
+
+# Requirements
+- Hollow Knight 1.4.3.2
+- Modding API 1.4.3.2-60
+
+## How to install
+
+Unzip the [last release](https://github.com/carloslancha/no-death-mod/releases/latest) .zip file in `HollowKnightPath/hollow_knight_Data/Managed/Mods` folder.
+
+## Preview
+
+https://user-images.githubusercontent.com/5803434/137586581-d4ffd9dd-07cc-4984-bade-f2eb98df2866.mp4
+
+## License
+
+BSD License © Carlos Lancha.<br/>
+www.twitch.tv/empaventuras

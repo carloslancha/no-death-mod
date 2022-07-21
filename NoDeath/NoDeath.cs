68e84c8e41cbcde515aa400623c926010bde54e4
Ezequiel A Stanganelli <ezequielstanganelli@gmail.com>
2022-07-21 12:20:51 -0300

    Reelase 1.5.78

---
+++ NoDeath/NoDeath.cs
@@ -0,0 +1,21 @@
+﻿using Modding;
+
+namespace NoDeath {
+    public class NoDeath : Mod {
+        private const string version = "1.5.78";
+        
+        public override void Initialize() {
+            ModHooks.TakeHealthHook += TakeHealth;
+        }
+        
+        public int TakeHealth(int damageAmount) {
+            int currentHealth = PlayerData.instance.health;
+
+            if (currentHealth - damageAmount < 1)
+                return 0;
+            
+            return damageAmount;
+        }
+        public override string GetVersion() => version;
+    }
+}
